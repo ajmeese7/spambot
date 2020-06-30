f6e6e2ae0da096cf993979b83e45ebc7eb3b7458
Aaron Meese <ajmeese7@gmail.com>
2020-06-30 07:57:50 -0500

    Cleaner code
    Instead of having everything crammed up into the main function, now it's just most of everything crammed into the main function.
    
    I added some useful error handling, improved the scoping of some variables, and got the counter for messages sent to display properly again.

--- bot.js
+++ bot.js
@@ -3,73 +3,44 @@ const client = new Discord.Client();
 const config = require('./config.json');
 client.config = config;
 
+setArgValues();
+process.on('unhandledRejection', error => {
+  console.error("Error trying to login with credentials! Did you update the config.json file?");
+  process.exit();
+});
+
 console.log("Ready to level up!");
+var count = 1; // Number of messages sent (modified by sendSpamMessage)
+var maxMessages = 10000;
+var timeToWait = null, minTime = 2000, maxTime = 4350;
+var content = null;
+var prune = false;
 
 try {
   client.on("message", async message => {
+    // Ignore message if the content doesn't apply to us
+    if (message.author.id !== client.user.id || message.content.indexOf(client.config.prefix) !== 0) return;
+
     const prefix = config.prefix;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    if (message.author.id !== client.user.id || message.content.indexOf(client.config.prefix) !== 0) return;
-
     if (command === "spam") {
-      var count = 1; // Number of messages sent (modified by sendSpamMessage)
-      var maxMessages = 10000; // Change based on how many messages you want sent
-      var timeToWait = null, minTime = 2000, maxTime = 4350;
-      var content = null;
-      var prune = false;
-
-      // Get command line arguments
-      var argLength = process.argv.length;
-      for (let j = 2; j < argLength; j++) {
-          // j is 2 initially to ignore `node bot.js`
-          var argsLeft = j + 1 < argLength;
-          var arg = process.argv[j];
-          var nextArg = process.argv[j + 1];
-
-          // All the flags require a second argument, so this only checks for flags if another arg exists
-          if (argsLeft) {
-            // TODO update docs and ensure proper typechecking and spit relevant error instead of running command
-            if (arg == "--message") {
-              content = nextArg;
-            } else if (arg == "--maxMessages") {
-              maxMessages = nextArg;
-            } else if (arg == "--setTime") {
-              timeToWait = nextArg;
-            } else if (arg == "--maxTime") {
-              // TODO ensure this isn't less than minTime
-              maxTime = nextArg;
-            } else if (arg == "--minTime") {
-              // TODO ensure this isn't greater than maxTime
-              minTime = nextArg;
-            }
-          }
-
-          // Doesn't require a second arg
-          if (arg == "--prune") {
-            prune = true;
-          }
-      }
-
       function sendSpamMessage() {
         // You could modify this to send a random string from an array (ex. a quote), create a
         // random sentence by pulling words from a dictionary file, or to just send a random
         // arrangement of characters and integers. Doing something like this may help prevent
         // future moderation bots from detecting that you sent a spam message.
         if (content === null) {
-          content = "This is spam message #" + count;
+          message.channel.send("This is spam message #" + count);
+        } else {
+          message.channel.send(content);
         }
-
-        message.channel.send(content);
-
+        
         if (count < maxMessages) {
           // If you don't care about whether the messages are deleted or not, like if you created a dedicated server
           // channel just for bot spamming, you can remove the below statement and the entire prune command.
-          if (prune) {
-            message.channel.send("/prune");
-          }
-          
+          if (prune) message.channel.send("/prune");
           count++;
 
           /* These numbers are good for if you want the messages to be deleted.
@@ -91,7 +62,7 @@ try {
         }
       }
 
-      message.delete().catch(O_o=>{})
+      message.delete().catch(O_o => { /* Into the abyss we go */ })
       sendSpamMessage();
     }
 
@@ -101,11 +72,48 @@ try {
         let message_array = messages.array();
         message_array.length = 2;
         message_array.map(msg => msg.delete().catch(O_o => {}));
-       });
+       }).catch(console.error);
     }
   });
 } catch (error) {
-  console.error("CAUGHT ERROR: " + error);
+  console.error("CAUGHT ERROR => " + error);
+}
+
+function setArgValues() {
+  // Get command line arguments
+  var argLength = process.argv.length;
+  for (let j = 2; j < argLength; j++) {
+    // j is 2 initially to ignore `node bot.js`
+    let argsLeft = j + 1 < argLength;
+    let arg = process.argv[j];
+    let nextArg = process.argv[j + 1];
+
+    // All the flags require a second argument, so this only checks for flags if another arg exists
+    if (argsLeft) {
+      // TODO update docs and ensure proper typechecking and spit relevant error instead of running command
+      if (arg == "--message") {
+        content = nextArg;
+      } else if (arg == "--maxMessages") {
+        maxMessages = nextArg;
+      } else if (arg == "--setTime") {
+        timeToWait = nextArg;
+      } else if (arg == "--maxTime") {
+        maxTime = nextArg;
+      } else if (arg == "--minTime") {
+        minTime = nextArg;
+      }
+    } else {
+      if (minTime && maxTime && minTime > maxTime) {
+        console.error("minTime can't be greater than maxTime!");
+        process.exit();
+      }
+    }
+
+    // Doesn't require a second arg
+    if (arg == "--prune") {
+      prune = true;
+    }
+  }
 }
 
 client.login(config.botToken);
